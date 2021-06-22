dd91ced44313038b1db9b6e41afbb3f9acd0f60c
Charlie Traylor <ctraylor@tql.com>
2021-06-22 16:23:06 -0400

    friends table angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Friends } from './friend.class';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'friends-list';
+  title = 'Friends List';
+
+    friends: Friends[] = 
+    [
+      new Friends('Mattea', 30, 'mmswain@tql.com', true),
+      new Friends('Jeff', 45, 'jnorth@tql.com', false),
+      new Friends('Z', 25, 'zpoindexter@tql.com', false),
+      new Friends('Matt', 30, 'mluedke@tql.com', false),
+      new Friends('John', 30, 'jadkins@tql.com', false)
+    ]
+    stripedrows: string = "table table-striped table-sm";
+
 }
---
+++ src/app/friend.class.ts
@@ -0,0 +1,16 @@
+export class Friends
+{
+    Name: string;
+    Age: number;
+    Email: string;
+    Bff: boolean;
+
+
+    constructor(name:string, age:number, email:string, bff:boolean)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Email = email;
+        this.Bff = bff;
+    }
+}
